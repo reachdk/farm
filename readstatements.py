ae282e26ea04ad12ad5a63da85675a2a3fd7a6af
reachdk <netmaildeepak@gmail.com>
2018-01-26 17:49:35 +0530

    All SBI consolidated in 1 file

--- readstatements.py
+++ readstatements.py
@@ -1,42 +1,25 @@
 import csv
-#import pandas as pd
+import pandas as pd
 import os
 import glob
 
-rawdata1 = []
+df = pd.DataFrame(columns=['Txn Date', 'Value Date', 'Description', 'Ref No', 'Debit', 'Credit', 'Balance'])
+'''
 directory = "/home/deepak/py/farm/source/sbi/"
 for file in os.listdir(directory):
     if file.endswith(".csv"):
-        with open(os.path.join(directory, file), 'r') as f:
-            reader = csv.reader(f)
-            rawdata = list(reader)
-            endrow = len(rawdata) - 2
-            rawdata1 = rawdata[20:endrow]
-            #rawdata2 = rawdata1.extend(rawdata)
-        f.close()
-print(rawdata1)
+        with open('farmSbiConsolidated.csv', 'a') as g:
+            with open(os.path.join(directory, file), 'r') as f:
+                reader = csv.reader(f)
+                rawdata = []
+                rawdata = list(reader)
+                endrow = len(rawdata) - 2
+                rawdata = rawdata[20:endrow]
+                rawdata_df = pd.DataFrame(rawdata)
+                rawdata_df.to_csv(g, header=False)
+            f.close()
+        g.close()
 '''
-with open('source/5808JanDec2017.csv', 'r') as f:
-    reader = csv.reader(f)
-    data5808 = list(reader)
-
-with open('source/7494JanDec2017.csv', 'r') as f:
-    reader = csv.reader(f)
-    data7494 = list(reader)
-
-with open('source/7494JanDec2017.csv', 'r') as f:
-    reader = csv.reader(f)
-    data7494 = list(reader)
-
-with open('source/7494JanDec2017.csv', 'r') as f:
-    reader = csv.reader(f)
-    data7494 = list(reader)
-
-with open('source/7494JanDec2017.csv', 'r') as f:
-    reader = csv.reader(f)
-    data7494 = list(reader)
-
-with open('source/7494JanDec2017.csv', 'r') as f:
-    reader = csv.reader(f)
-    data7494 = list(reader)
-'''
+farmConsolidated = pd.read_csv('farmSbiConsolidated.csv', header=None, names=['Txn Date', 'Value Date', 'Description', 'Ref No', 'Debit', 'Credit', 'Balance'])
+farmConsolidated.to_csv('consolidatedWithHeaders.csv')
+#print(df[0])
